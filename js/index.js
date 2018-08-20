a41a98e3747760fb90025d8b73bc181cabd18d30
agaghd <15150829710@163.com>
2018-08-20 22:29:27 +0800

    try something

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 var app = new Framework7({
 	//App root element 
-	root : '#app',
-	name : 'Framework7 Demo',
-	id	 : 'agaghd.github.io',
-	
+	root: '#app',
+	name: 'Framework7 Demo',
+	id: 'agaghd.github.io',
+	// Enable swipe panel 让侧边栏可以滑动
+	panel: {
+		swipe: 'left',
+	},
+});
+var $$ = Dom7;
+//点击事件样例
+$$('#clickExample').on('click', function(e) {
+	//alert 警告性质的对话框
+	app.dialog.alert("Hi!");
 });
